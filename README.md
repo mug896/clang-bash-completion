012cab89cb8c4b49348de417a3da5e3da1200a06
Hyunho Cho <mug896@gmail.com>
2022-11-08 22:20:27 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# clang-bash-completion
-A bash script to complete a command line of clang 
+# Clang Bash Completion
+
+This is a clang bash completion function that uses the same `clang --autocomplete="str"` 
+command as the original function for generating completion words.
+so there is no difference in the results.
+just added a few features.
+
+For example, you can search for completion words using `*`, `?`, `[...]` glob characters
+while writing command line.
+
+```sh
+bash$ clang -save-temps -*strict*[tab]
+. . .
+-fstrict-aliasing
+-fstrict-enums
+-fstrict-flex-arrays=
+-fstrict-float-cast-overflow
+-fstrict-overflow
+-fstrict-return
+-fstrict-vtable-pointers
+. . .                       # "q"
+[tab][tab]                  # [tab][tab] to exit to the prompt.
+
+---------------------------------------------------------
+
+bash$ clang -save-temps -*strict*[tab]
+. . .
+-fno-strict-aliasing
+--strict-aliasing
+. . .                          # "q"
+[backspace]fno-stric[tab]      # one backspace key is needed.
+or
+[backspace]-strict-ali[tab]    # one "-" char in front is hidden.
+```
+
+
+## Installation
+
+Copy contents of gcc-bash-completion.sh to ~/.bash_completion  
+open new terminal and try auto completion !
+
+
+> please leave an issue above if you have any problems using this script.
---
+++ clang-bash-completion.sh
@@ -0,0 +1,90 @@
+_init_comp_wordbreaks()
+{
+    if [[ $PROMPT_COMMAND == *";COMP_WORDBREAKS="* ]]; then
+        [[ $PROMPT_COMMAND =~ ^:\ ([^;]+)\; ]]
+        [[ ${BASH_REMATCH[1]} != "${COMP_WORDS[0]}" ]] && eval "${PROMPT_COMMAND%%$'\n'*}"
+    fi
+    if [[ $PROMPT_COMMAND != *";COMP_WORDBREAKS="* ]]; then
+        PROMPT_COMMAND=": ${COMP_WORDS[0]};COMP_WORDBREAKS=${COMP_WORDBREAKS@Q};\
+        "$'PROMPT_COMMAND=${PROMPT_COMMAND#*$\'\\n\'}\n'$PROMPT_COMMAND
+    fi
+}
+_clang() 
+{
+    # It is recommended that every completion function starts with _init_comp_wordbreaks,
+    # whether or not they change the COMP_WORDBREAKS variable afterward.
+    _init_comp_wordbreaks
+    COMP_WORDBREAKS=${COMP_WORDBREAKS//:/}
+    [[ $COMP_WORDBREAKS != *","* ]] && COMP_WORDBREAKS+=","
+
+    local IFS=$' \t\n' CUR CUR_O PREV PREV_O PREV2 PREO
+    local CMD=$1 CMD2 WORDS COMP_LINE2 HELP args arr i v
+
+    CUR=${COMP_WORDS[COMP_CWORD]} CUR_O=$CUR
+    [[ ${COMP_LINE:COMP_POINT-1:1} = " " || $COMP_WORDBREAKS == *$CUR* ]] && CUR=""
+    PREV=${COMP_WORDS[COMP_CWORD-1]} PREV_O=$PREV
+    [[ $PREV == [,=] ]] && PREV=${COMP_WORDS[COMP_CWORD-2]}
+    if (( COMP_CWORD > 4 )); then
+        [[ $CUR_O == [,=] ]] && PREV2=${COMP_WORDS[COMP_CWORD-3]} || PREV2=${COMP_WORDS[COMP_CWORD-4]}
+    fi
+    COMP_LINE2=${COMP_LINE:0:$COMP_POINT}
+    eval arr=( $COMP_LINE2 ) 2> /dev/null
+    for (( i = ${#arr[@]} - 1; i > 0; i-- )); do
+        if [[ ${arr[i]} == -* ]]; then
+            PREO=${arr[i]%%[^[:alnum:]_-]*}
+            [[ ($PREO == ${COMP_LINE2##*[ ]}) && ($PREO == $CUR_O) ]] && PREO=""
+            break
+        fi
+    done
+
+    if [[ $PREO == @(-Wl|-Wa) ]]; then
+        [[ $PREO == -Wl ]] && CMD2="ld.lld" || CMD2="as"    # CMD2=ld.lld
+        HELP=$( $CMD2 --help 2> /dev/null )
+
+        if [[ $CUR == -* ]]; then
+            WORDS=$(<<< $HELP sed -En '
+            s/^\s{,3}((-[^ ,=]+([ =][^ ,]+)?)(, *-[^ ,=]+([ =][^ ,]+)?)*)(.*)/\1/g; tX;
+            b; :X s/((^|[^[:alnum:]])-[][[:alnum:]_+-]+=?)|./\1 /g; 
+            s/[,/ ]+/\n/g; s/\[=$/=/Mg; s/\[[[:alnum:]-]+$//Mg;  
+            :Y h; tR1; :R1 s/([^=]+)\[(\|?(\w+-?))+](.*)/\1\3\4/; p; tZ; b; 
+            :Z g; s/\|?\w+-?]/]/; tR2 :R2 s/-\[]([[:alnum:]])/-\1/p; tE; /\[]/! bY :E' )
+
+        elif [[ $PREO == -Wl && $PREV == -z ]]; then
+            WORDS=$(<<< $HELP sed -En 's/^\s*-z ([[:alnum:]-]+=?).*/\1/p' )
+        
+        elif [[ ($PREV == -* && $PREV != $PREO) || $PREV2 == -z ]]; then
+            WORDS=$(<<< $HELP sed -En 's/.* '"$PREV"'[ =]\[([^]]+)].*/\1/; tX; b; :X s/[,|]/\n/g; p')
+        fi
+
+    elif [[ $CUR == -* ]]; then
+        if [[ $CUR == *[*?[]* ]]; then
+            WORDS=$( $CMD --autocomplete="-" | sed -E 's/([ \t=]).*$/\1/' )
+            declare -A aar; IFS=$'\n'; args=; echo
+            for v in $WORDS; do 
+                let aar[$v]++
+                if [[ $v == $CUR && ${aar[$v]} -eq 1 ]]; then
+                    echo -e "\\e[36m$v\\e[0m"
+                fi
+            done | less -FRSXi
+            args="-"
+            IFS=$'\n' COMPREPLY=($( compgen -W "$args" ))
+
+        else
+            WORDS=$( $CMD --autocomplete="$CUR" | gawk '{print $1}' )
+            WORDS+=$'\n--autocomplete='
+        fi
+
+    elif [[ $PREV == -[[:alnum:]-]* ]]; then
+        [[ $CUR_O == "=" || $PREV_O == "=" ]] && args="$PREV=" || args="$PREV"
+        WORDS=$( $CMD --autocomplete="$args" | gawk '{print $1}' )
+        [[ $PREV == $WORDS ]] && WORDS=
+    fi
+
+    if [[ -z $COMPREPLY ]]; then
+        WORDS=$( <<< $WORDS sed -E 's/^[[:blank:]]+|[[:blank:]]+$//g' )
+        IFS=$'\n' COMPREPLY=($(compgen -W "$WORDS" -- "$CUR"))
+    fi
+    [[ ${COMPREPLY: -1} == [=,] ]] && compopt -o nospace
+}
+
+complete -o default -o bashdefault -F _clang clang clang++
